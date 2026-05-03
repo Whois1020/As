aad8e84abf68692e284824e50dd831734cd467e2
Whois1020 <whoisgithun@gmail.com>
2026-05-03 00:05:57 -0500

    Actualizar group-fankicks.js

--- plugins/group-fankicks.js
+++ plugins/group-fankicks.js
@@ -19,6 +19,6 @@ handler.tags = ['grupo']
 handler.command = ['kick', 'echar', 'hechar','sacar', 'ban']
 handler.admin = true
 handler.group = true
-handler.botAdmin = false
+handler.botAdmin = true
 
 export default handler
