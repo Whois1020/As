03841043844df0c1e9148e1edfb4908919fc1c1a
Whois1020 <whoisgithun@gmail.com>
2026-05-03 00:05:39 -0500

    Actualizar group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -35,7 +35,7 @@ handler.tags = ['group'];
 handler.command = ['demote', 'degradar'];
 handler.group = true;
 handler.admin = true;
-handler.botAdmin = false;
+handler.botAdmin = true;
 handler.fail = null;
 
 export default handler;
