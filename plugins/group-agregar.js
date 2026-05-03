c5b8b5ffc9383fdbcba3213ccfe51d086d13e418
Whois1020 <whoisgithun@gmail.com>
2026-05-03 00:04:45 -0500

    Actualizar group-agregar.js

--- plugins/group-agregar.js
+++ plugins/group-agregar.js
@@ -43,7 +43,7 @@ handler.tags = ['group'];
 handler.command = ['add', 'agregar', 'añadir'];
 handler.group = true;
 handler.admin = true;
-handler.botAdmin = false;
+handler.botAdmin = true;
 handler.fail = null;
 
 export default handler;
